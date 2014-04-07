988affd5d182f012b8b9446adb8a866fae94bfc8
zuzhi1900 <legacylies@gmail.com>
2014-04-07 16:55:07 +0800

    Initial Commit

---
+++ AgainToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AgainToDoList
+//
+//  Created by zuzhi on 14-4-7.
+//  Copyright (c) 2014年 zuzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AgainToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AgainToDoList
+//
+//  Created by zuzhi on 14-4-7.
+//  Copyright (c) 2014年 zuzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
